60600dc43d8df152cd2bdf0fe0f8f8f5db4d0c21
Jelena Jovicic <jejovicic@gmail.com>
2022-01-22 16:50:38 +0100

    Work on menu and services section

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 // hamburger
 
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".nav-menu");
+const closeIcon= document.querySelector(".close-icon");
+const menuIcon = document.querySelector(".menu-icon");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", toggleMenu);
+
+navLink.forEach(n => n.addEventListener("click", hideMenu));
+
+function toggleMenu() {
+    if (menu.classList.contains("clicked")) {
+      hamburger.setAttribute("aria-expanded", "false");
+      menu.classList.remove("clicked");
+      closeIcon.style.display = "none";
+      menuIcon.style.display = "block";
+    } else {
+      menu.classList.add("clicked");
+      closeIcon.style.display = "block";
+      menuIcon.style.display = "none";
+      hamburger.setAttribute("aria-expanded", "true");
+    }
+  }
+
+function hideMenu () {
+    hamburger.setAttribute("aria-expanded", "false");
+    menu.classList.remove("clicked");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+}
+
 // email validation
